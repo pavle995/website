7024b0177e21c16bdd6f8c7ff7ce0599782b085a
pavle995 <kosuticp95@gmail.com>
2022-11-08 22:50:07 +0100

    test deploy

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="text-3xl font-bold pt-40 text-center">
-      <h1 >Hey there, I&apos;m Pavle Kosutic</h1>
+      <h1 >Hey there, I&apos;m Pavle Kosutic...</h1>
     </div>
   )
 }
