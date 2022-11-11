358f1cf2c7b7d31f0bc0159ebb6b27a7c295c777
pavle995 <kosuticp95@gmail.com>
2022-11-11 12:36:49 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="">
         <img src="https://public-assets-pk.s3.eu-south-1.amazonaws.com/profile.jpeg" className="mx-auto max-w-full h-auto rounded-full pt-10 object-center object-contain h-64 w-64" alt=""/>
       </div>
-      <h1 className="text-3xl font-bold pt-10">Hey there, I&apos;m Pavle Kosutic</h1>
+      <h1 className="text-3xl font-bold pt-10">Hey there, I&apos;m Pavle Kosutic...</h1>
       <div className="inline-flex pt-5">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-5 h-5"><path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/></svg>
         <a href="https://www.linkedin.com/in/pavle-kosutic/" className="font-medium text-blue-600 dark:text-blue-500 hover:underline px-2">My LinkedIn Profile</a>
