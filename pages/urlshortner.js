d7f4bbab98ea9e5b549f08e17d564a5e530f8004
pavle995 <kosuticp95@gmail.com>
2022-11-24 12:29:06 +0100

    Revert "removed nextjs api call"
    This reverts commit d5239a5d0e21f268c913fbb6e00162b6ca7d732d.

--- pages/urlshortner.js
+++ pages/urlshortner.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import useSWR from 'swr'
 import Link from 'next/link'
 
+const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function UrlShortner() {
     const [url, setUrl] = useState("")
@@ -8,25 +10,14 @@ export default function UrlShortner() {
 
 
     function handleClick() {
-        try {
-            const payload = new URLSearchParams();
-            payload.append('url', url)
-            fetch(`http://ec2-15-161-149-189.eu-south-1.compute.amazonaws.com/short`,
-                {
-                    method: 'POST',
-                    body: payload,
-                    mode: 'no-cors'
-                })
-                .then((res) => res.json())
-                .then((data) => {
-                    setShortUrl(data.shortUrl)
-                })
-            console.log(shortUrl)
-            const shortUrl = "http://ec2-15-161-149-189.eu-south-1.compute.amazonaws.com/".concat(data.shortUrl)
-            setShortUrl(shortUrl)
-        } catch (err) {
-            console.log(err)
-        }
+        console.log("hello there")
+        fetch('/api/urlshortner', {
+            method: "POST", body: JSON.stringify({url: url})
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setShortUrl(data.url)
+            }) 
     }
 
     function handleChange(event) {
@@ -40,7 +31,7 @@ export default function UrlShortner() {
                             <label htmlFor="url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Long URL</label>
                             <input type="url" id="url" value={url} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://www.example.com" required />
                         </div>
-                        <button onClick={() => {handleClick()}} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <button onClick={handleClick} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                     </div>
                     <label>
                         Short url:
